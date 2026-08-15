821858ed89a4935e95f8113860c8e45d6c56a09e
秋水锦瑟 <38565188+roseion@users.noreply.github.com>
2026-08-15 13:02:05 +0800

    daily: 更新史低数据 2026-08-15

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-window.__GAMES__ = {"generatedAt":"2026-08-15T05:01:12.886Z","source":"https://www.yxdzqb.com/","total":0,"break_":[],"tie":[]};
+window.__GAMES__ = {"generatedAt":"2026-08-15T05:01:43.127Z","source":"https://www.yxdzqb.com/","total":0,"break_":[],"tie":[]};
